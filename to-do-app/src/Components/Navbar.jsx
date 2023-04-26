55daa0d6b034ff14f3d8bcd934f288ddeb2cecdb
Tayyab <tayyabilyas963@gmail.com>
2023-04-26 07:17:54 +0530

    basic fxns

--- to-do-app/src/Components/Navbar.jsx
+++ to-do-app/src/Components/Navbar.jsx
@@ -23,11 +23,6 @@ export default function Navbar(props) {
       {props.searchBar?<input type="text" id="search-navbar" className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..."/>
     :""}
     </div>
-    <button data-collapse-toggle="navbar-search" type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-search" aria-expanded="false">
-      <span className="sr-only">Open menu</span>
-      <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
-    </button>
-  </div>
     <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-search">
       <div className="relative mt-3 md:hidden">
         <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
@@ -39,12 +34,11 @@ export default function Navbar(props) {
         <li>
           <a href="#" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</a>
         </li>
-        <li>
-          <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
-        </li>
+       
         
       </ul>
     </div>
+  </div>
   </div>
 </nav>
 
