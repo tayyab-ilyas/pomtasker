68709d06f72c15a55e7e1a174e9bce714c480d5c
Tayyab <tayyabilyas963@gmail.com>
2023-04-27 03:48:29 +0530

    minor change

--- pomtasker-app/src/Components/About.jsx
+++ pomtasker-app/src/Components/About.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>looooooooooooool</div>
+    <div></div>
   )
 }
--- pomtasker-app/src/Components/Footer.jsx
+++ pomtasker-app/src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <>
-    <footer className="relative mt-[20vh] w-full bg-gray-800">
+    <footer className="bottom-0 absolute w-full bg-gray-800">
         <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© Tayyab Ilyas
         </span>
